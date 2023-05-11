de716b6a2e5d6f22797b8310b3dcf6cfaa280922
Oleg Sichev <sichev.oleg@bk.ru>
2023-05-11 12:07:20 +0300

    homework 2 completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,39 @@ public class Main {
                 ) {
                     System.out.println("New connection accepted");
                     final String name = in.readLine();
-                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                    out.println(String.format("Hi %s, your port is %d. Are you student?", name, clientSocket.getPort()));
+                    String message = in.readLine();
+                    if (message.equals("y")){
+                        out.println("Oh, are you studying java? y/n");
+                        message = in.readLine();
+                        if (message.equals("y")){
+                            out.println("That's cool! Excellent studies! How old are you?");
+                            message = in.readLine();
+                            int messageInt = Integer.parseInt(message);
+                            if (messageInt <= 18 && messageInt >= 5){
+                                out.println("Oh! You so young student! That's cool! Good luck! Bye! Off");
+                                break;
+                            } else if (messageInt >= 18) {
+                                out.println("It's never too late to start learning! I wish you good luck! Off");
+                                break;
+                            } else {
+                                out.println("Your age is incorrect. You're trying to deceive me! Goodbye. Off");
+                                break;
+                            }
+                        } else if (message.equals("n")) {
+                            out.println("It's sad! You need to study java! Off");
+                            break;
+                        } else {
+                            out.println("Your age is incorrect. You're trying to deceive me! Goodbye. Off");
+                            break;
+                        }
+                    } else if (message.equals("n")){
+                        out.println("It's bad! It's very very bad! I only want to talk to students. Bye. Off");
+                        break;
+                    } else {
+                        out.println("Your age is incorrect. You're trying to deceive me! Goodbye. Off");
+                        break;
+                    }
                 }
             }
         }
